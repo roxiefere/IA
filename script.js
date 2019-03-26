fab597e37b20cfd359283403e6c563651c328be9
Roxanne Ferentz <roxanneferentz@students.berkeley.net>
2019-03-25 21:26:50 -0700

    hey

--- script.js
+++ script.js
@@ -20,8 +20,103 @@ function makeOutfits() {
     var otherT = parseInt(document.getElementById("OtherType").value);
     var otherC = parseInt(document.getElementById("OtherColor").value);
     var otherP = parseInt(document.getElementById("OtherPattern").value);
-    if ((10101 <= (shirtT + shirtC + shirtP) <= 10211) || (10801 <= (shirtT + shirtC + shirtP) <= 10811)) {
-        document.getElementById("result").innerHTML = "You should wear skinny jeans, shorts, overalls, ripped jeans, leggings, or sweat pants with this shirt.";
+   //similar shirt types
+    if(shirtT == 1 || 5 || 6){
+        shirtT == 1000;
+    }
+    if(shirtT == 3 || 9 || 10){
+        shirtT == 2000;
+    }
+    if(shirtT == 2 || 4){
+        shirtT == 3000;
+    }
+    if(shirtT == 11 || 12){
+        shirtT == 4000;
+    }
+    //similar pant types
+    if(pantT == 1 || 7){
+        pantT == 1000;
+    }
+    if(pantT == 4 || 5){
+        pantT == 2000;
+    }
+    if(pantT == 8 || 10){
+        pantT == 3000;
+    }
+    //similar other types
+    if(otherT == 5 || 6){
+        otherT == 5000;
+    }
+    if(otherT == 7 || 8){
+        otherT == 6000;
+    }
+    //colors that go together
+    if(shirtC == 1 || 2 || 8){
+        shirtC == 100;
+    }
+    if(pantC == 1 || 2 || 8){
+        pantC == 100;
+    }
+    if(otherC == 1 || 2 || 8){
+        otherC == 100;
+    }
+    if(shirtC == 3 || 4){
+        shirtC == 200;
+    }
+    if(pantC == 3 || 4){
+        pantC == 200;
+    }
+    if(otherC == 3 || 4){
+        otherC == 200;
+    }
+    if(shirtC == 5 || 6 || 7){
+        shirtC == 300;
+    }
+    if(pantC == 5 || 6 || 7){
+        pantC == 300;
+    }
+    if(otherC == 5 || 6 || 7){
+        otherC == 300;
+    }
+    if(shirtC == 9 || 10 || 11 || 12){
+        shirtC == 400;
+    }
+    if(pantC == 9 || 10 || 11 || 12){
+        pantC == 400;
+    }
+    if(otherC == 9 || 10 || 11 || 12){
+        otherC == 400;
+    }
+    //patterns that go together
+    if(shirtP == 1 || 6){
+        shirtP == 10;
+    }
+    if(pantP == 1 || 6){
+        pantP == 10;
+    }
+    if(otherP == 1 || 6){
+        otherP == 10;
+    }
+    if(shirtP == 2 || 3 || 4 || 5 || 10){
+        shirtP == 20;
+    }
+    if(pantP == 2 || 3 || 4 || 5 || 10){
+        pantP == 20;
+    }
+    if(otherP == 2 || 3 || 4 || 5 || 10){
+        otherP == 20;
+    }
+    if(shirtP == 8 || 9 || 11){
+        shirtP == 30;
+    }
+    if(pantP == 8 || 9 || 11){
+        pantP == 30;
+    }
+    if(otherP == 8 || 9 || 11){
+        otherP == 30;
+    }
+    if (1110 <= (shirtT + shirtC + shirtP) <= 1450) {
+        document.getElementById("result").innerHTML = "You should wear skinny jeans, jean shorts, or ripped jeans with this/these shirt(s).";
     }
     if (10301 <= (shirtT + shirtC + shirtP) == 10411) {
         document.getElementById("result").innerHTML = "You should wear ";
